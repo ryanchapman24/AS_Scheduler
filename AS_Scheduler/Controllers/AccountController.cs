60b4ad8dad0e383820e4c3e14945bebabda3ba97
Ryan Chapman <chapman.ryansadler@gmail.com>
2017-01-13 16:46:49 -0500

    week end 1/13/17

--- AS_Scheduler/Controllers/AccountController.cs
+++ AS_Scheduler/Controllers/AccountController.cs
@@ -180,16 +180,17 @@ namespace Scheduler.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    //await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                    var helper = new UserRolesHelper(db);
+                    helper.AddUserToRole(user.Id, "User");
+                    db.SaveChanges();
+
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
                     //// For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     //// Send an email with this link
                     //string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     //var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     //await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    var helper = new UserRolesHelper(db);
-                    helper.AddUserToRole(user.Id, "User");
 
                     return RedirectToAction("Index", "Home");
                 }
